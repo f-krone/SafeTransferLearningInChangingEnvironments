5e7b675cba1dcdaf21f40fff8542cb0b9eaba24c
Florian Krone <f-krone@gmx.de>
2022-05-19 12:13:20 +0200

    fixed gitignore

---
+++ src/sac_ae/env/env.py
@@ -0,0 +1,70 @@
+import imp
+import dmc2gym
+import gym
+import numpy as np
+from collections import deque
+import custom_robotics
+
+def make_envs(args):
+    """env = dmc2gym.make(
+        domain_name=args.domain_name,
+        task_name=args.task_name,
+        seed=args.seed,
+        visualize_reward=False,
+        from_pixels=True,
+        height=args.env_image_size,
+        width=args.env_image_size,
+        frame_skip=args.action_repeat
+    )"""
+    env = gym.make(args.domain_name + '-' + args.task_name)
+    max_episode_steps = env._max_episode_steps
+    env = PixelObservation(
+        env,
+        height=args.env_image_size,
+        width=args.env_image_size)
+    env.seed(args.seed)
+    env = FrameStack(env, k=args.frame_stack, max_episode_steps=max_episode_steps)
+    return env
+
+class PixelObservation(gym.ObservationWrapper):
+
+    def __init__(self, env, width=128, height=128):
+        super(gym.ObservationWrapper, self).__init__(env)
+        self.width = width
+        self.height = height
+        low, high = (0, 255)
+        self.observation_space = gym.spaces.Box(shape=(3, height, width), low=low, high=high, dtype=np.uint8)
+
+    def observation(self, observation):
+        obs = self.env.render(mode='rgb_array', width=self.width, height=self.height)
+        obs = obs.transpose(2, 0, 1).copy()
+        return obs
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k, max_episode_steps):
+        gym.Wrapper.__init__(self, env)
+        self._k = k
+        self._frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=1,
+            shape=((shp[0] * k,) + shp[1:]),
+            dtype=env.observation_space.dtype
+        )
+        self._max_episode_steps = max_episode_steps
+
+    def reset(self):
+        obs = self.env.reset()
+        for _ in range(self._k):
+            self._frames.append(obs)
+        return self._get_obs()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self._frames.append(obs)
+        return self._get_obs(), reward, done, info
+
+    def _get_obs(self):
+        assert len(self._frames) == self._k
+        return np.concatenate(list(self._frames), axis=0)
